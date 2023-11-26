f6de1ed85150993f55557f1e4d70d0cca5d0e7e0
zhorik07 <130290013+khizhniak07@users.noreply.github.com>
2023-11-26 22:37:07 +0100

    three

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('#vimeo-player');
+const iframe1 = document.querySelector('iframe');
+console.log(iframe1);
+
+const play = new Player(iframe);
+play.on('timeupdate', throttle(fnTime, 1000));
+
+function fnTime(data) {
+  let time = data.seconds;
+  localStorage.setItem('videoplayer-current-time', time);
+}
+const saveTime = localStorage.getItem('videoplayer-current-time');
+if (saveTime) {
+  play.setCurrentTime(saveTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+const throttle = require('lodash.throttle');
+const form = document.querySelector('.feedback-form');
+const { email, message } = form.elements;
+
+form.addEventListener(
+  'input',
+  throttle(handlerObject, 500, { leading: true, trailing: true })
+);
+
+function handlerObject(evt) {
+  const objForm = {
+    userEmail: email.value,
+    userMessage: message.value,
+  };
+  if (evt.target.name === 'email') {
+    objForm.userEmail = evt.target.value;
+  }
+  if (evt.target.name === 'message') {
+    objForm.userMessage = evt.target.value;
+  }
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(objForm));
+}
+
+const localText = JSON.parse(localStorage.getItem('feedback-form-state'));
+if (localText) {
+  autoText(localText);
+}
+
+function autoText(objText) {
+  const { userEmail, userMessage } = objText;
+  email.value = userEmail;
+  message.value = userMessage;
+}
+
+form.addEventListener('submit', handlerSubmit);
+
+function handlerSubmit(event) {
+  event.preventDefault();
+
+  const lastLog = JSON.parse(localStorage.getItem('feedback-form-state'));
+  console.log(lastLog);
+
+  localStorage.removeItem('feedback-form-state');
+  email.value = '';
+  message.value = '';
+}
 
